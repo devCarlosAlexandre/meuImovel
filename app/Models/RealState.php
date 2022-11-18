f1f321da25abecf1508eb4db11b8316e91f8c718
devCarlosALexandre <carloalexandresousa@gmail.com>
2022-11-18 15:00:49 -0300

    constructor fisrt endpoint

---
+++ app/Models/RealState.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RealState extends Model
+{
+    use HasFactory;
+    protected $table = 'real_state';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\HTTP\Controllers\Api\RealStateController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('/real-states')->name('real_states.')->group(function () {
+
+        Route::get('/', [RealStateController::class, 'index']);
+
+    });
 });
